083068ada213bbe8502c9fc7ae98ca77e969b7e8
IslamWalid <islamwalid83@gmail.com>
2022-11-07 19:12:10 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 set -e
-cd /tmp
 wget https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
 chmod a+x busybox
 mkdir -p rootfs/bin rootfs/proc rootfs/dev
 ./busybox --install rootfs/bin
+mv rootfs /tmp
 rm busybox
